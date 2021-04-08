b5edf434e2615bb567bce3b5376d123a10f5b149
vishalkothari <vdkothari@tekion.com>
2021-04-08 17:45:21 +0530

    Initial Commit for the Project

---
+++ src/main/java/com/vishal/vkiit/VkiitApplication.java
@@ -0,0 +1,13 @@
+package com.vishal.vkiit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkiitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkiitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vishal/vkiit/VkiitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishal.vkiit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkiitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
