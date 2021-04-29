0fa4da03f36292366423def4d6330b12d10b4c88
vishalkothari <vdkothari@tekion.com>
2021-04-29 11:01:15 +0530

    Adding Entity Relationships.

--- src/main/java/com/vishal/vkiit/domain/Comment.java
+++ src/main/java/com/vishal/vkiit/domain/Comment.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Data
@@ -15,5 +16,8 @@ public class Comment {
     @GeneratedValue
     private Long id;
     private String body;
+
+    @ManyToOne
+    private Link link;
 
 }
